3e4407ae912ceb22473063a3a6d0aac758dd5f8b
Matt Hwang <mhwang@goshippo.com>
2023-03-05 13:33:33 -0800

    Test inventory service get

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState, createElement } from "react";
+import { useState, createElement, useEffect } from "react";
 import { Session, useSupabaseClient } from "@supabase/auth-helpers-react";
 // import {
 //   MenuFoldOutlined,
@@ -10,16 +10,22 @@ import { Session, useSupabaseClient } from "@supabase/auth-helpers-react";
 // import { Layout, Menu, Space, theme } from "antd";
 import styles from "./dashboard.module.css";
 import Link from "next/link";
+import { TokenService } from "../../services/supabase/TokenService";
+import { InventoryService } from "../../services/amazon/InventoryService";
 
 // const { Header, Sider, Content } = Layout;
 
-export default function Dashboard({ session }: { session: Session }) {
+function Dashboard({ session, data }: { session: Session; data: any }) {
   const supabase = useSupabaseClient();
 
   const [collapsed, setCollapsed] = useState(false);
   //   const {
   //     token: { colorBgContainer },
   //   } = theme.useToken();
+
+  useEffect(() => {
+    console.log(data);
+  }, []);
 
   return (
     <div>
@@ -85,3 +91,27 @@ export default function Dashboard({ session }: { session: Session }) {
     // </Layout>
   );
 }
+
+// This gets called on every request
+export async function getServerSideProps() {
+  const supabase = useSupabaseClient();
+  let data;
+  console.log("user is ");
+
+  const user = await supabase.auth.getUser();
+  const u = user.data.user;
+  const ts = new TokenService(supabase);
+  const token = await ts.getRefreshTokenByRegion({ region: "na", user: u! });
+  const i = new InventoryService();
+  const inv = await i.getInventorySummary({
+    region: "na",
+    marketplaceId: "ATVPDKIKX0DER",
+    refreshToken: token?.refresh_token,
+  });
+  data = "heyo";
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export default Dashboard;
