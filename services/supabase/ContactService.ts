805b061d3424ff44f6e79b99db15588357cdae01
Matt Hwang <mhwang@goshippo.com>
2023-04-04 19:50:38 -0700

    Remove copypasta

--- services/supabase/ContactService.ts
+++ services/supabase/ContactService.ts
@@ -39,44 +39,4 @@ export class ContactService {
 
     return data;
   }
-
-  async saveProductData(args: SaveProductData) {
-    const product = args.product;
-    const { data, error } = await this.supabase
-      .from('product_items')
-      .insert([
-        {
-          name: product.name,
-          asin: product.asin,
-          sku: product.sellerSku,
-          fnsku: product.fnSku,
-          user_id: args.user.id,
-        },
-      ])
-      .select('id');
-    if (error) {
-      console.log(error);
-      return error;
-    }
-
-    const { data: qData, error: qError } = await this.supabase
-      .from('amz_item_quantities')
-      .insert([
-        {
-          item_id: data[0].id,
-          amz_fulfillable: args.quantities.amzFulfillable,
-          amz_inbound_working: args.quantities.amzInboundWorking,
-          amz_inbound_shipped: args.quantities.amzInboundShipped,
-          amz_inbound_receiving: args.quantities.amzInboundReceiving,
-          amz_total: args.quantities.amzTotal,
-          user_id: args.user.id,
-        },
-      ]);
-    if (qError) {
-      console.log(qError);
-      return qError;
-    } else {
-      return data;
-    }
-  }
 }
