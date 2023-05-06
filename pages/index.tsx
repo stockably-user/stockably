a7ac78f8a857f2119a7963f87c713f164e223942
Paulo Ramos <pauloramos64@gmail.com>
2023-05-06 12:00:33 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import Head from 'next/head';
-import { useSession } from '@supabase/auth-helpers-react';
-import Dashboard from './dashboard';
-import Login from './login';
-import { AppShell, Box, Header, Text } from '@mantine/core';
-import CustomNavbar from '../components/nav/Navbar';
-import StockablyAppShell from '../components/nav/StockablyAppShell';
+import Head from "next/head";
+import { useSession } from "@supabase/auth-helpers-react";
+import Dashboard from "./dashboard";
+import Login from "./login";
+import { AppShell, Box, Header, Text } from "@mantine/core";
+import CustomNavbar from "../components/nav/Navbar";
+import StockablyAppShell from "../components/nav/StockablyAppShell";
 
 function Home() {
   const session = useSession();
+  console.log("test");
 
   return (
     <div>
